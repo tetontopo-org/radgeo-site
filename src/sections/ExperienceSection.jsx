715be40cefbc87af6feabb3a0ce3fddc6f39b4b3
Ian Surat-Mosher <iansmosher1@gmail.com>
2025-11-03 16:37:20 -0700

    working on experience section and soon shiny cards

---
+++ src/sections/ExperienceSection.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import TitleHeader from "../components/TitleHeader";
+import { expCards } from "../constants";
+
+const ExperienceSection = () => {
+  return (
+    <section
+      id="experience"
+      className="w-full md:mt-40 mt-20 section-padding xl:px-0"
+    >
+      <div className="w-full h-full md:px-20 px-5">
+        <TitleHeader title="Our Experience" sub="🎅🏽 Something Subtitley" />
+
+        <div className="mt-32 relative">
+          <div className="relative z-50 xl:space-y-32 space-y-10">
+            {expCards.map((card) => {
+              <div key={card.title} className="exp-card-wrapper">
+                {card.title}
+              </div>;
+            })}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default ExperienceSection;
